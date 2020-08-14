ada5a2847fd4bb4137093f2c27fec5f42786b62a
xiangyulong <yulong.xiang@aqara.com>
2020-08-14 17:19:38 +0800

    expect

--- deploy1.sh
+++ deploy1.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env expect
 
 # 确保脚本抛出遇到的错误
-set -e
+# set -e
 
 # 生成静态文件
-npm run build
+spawn npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+spawn cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
