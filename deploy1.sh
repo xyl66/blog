9d83031c66edcd1cf083525e23504893396fcf6a
xiangyulong <yulong.xiang@aqara.com>
2020-08-14 17:08:56 +0800

    expect

---
+++ deploy1.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env expect
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+spawn scp -r docs/.vuepress/dist/ root@132.232.60.166:~/static/doc/;
+expect {
+    *yes/no* { send "yes"; exp_continue }
+    *password:* { send "x$7IMCE%3n0$ZZmH" }  
+  }
+interact
+
+cd -
